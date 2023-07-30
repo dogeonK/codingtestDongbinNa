ad115d0872ad171b2f862614df6e228409dc41b4
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-07-30 17:15:49 +0900

    [ADD] binary_search

---
+++ binary_search/bs_recursive.py
@@ -0,0 +1,24 @@
+def binary_search(arrary, target, start, end):
+    if start > end:
+        return None
+    
+    mid = (start + end) // 2
+
+    if arrary[mid] == target:
+        return mid
+    elif arrary[mid] > target:
+        return binary_search(arrary, target, start, mid - 1)
+    else:
+        return binary_search(arrary, target, mid + 1, end)
+
+# 원소의 개수 n, 찾고자 하는 값 target
+n, target = list(map(int, input().split()))
+
+array = list(map(int, input().split()))
+
+result = binary_search(array, target, 0, n - 1)
+
+if result == None:
+    print("원소가 존재하지 않음")
+else:
+    print("해당 원소 {}은 {}번째에 있으며, 인덱스 값은 {}입니다.".format(target, result + 1, result))
